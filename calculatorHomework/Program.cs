8db1637da928ad9fbcb3f47d54348afce56d8d83
Ivan <kuzmenko.ivan.qa@gmail.com>
2025-02-02 15:55:41 +0100

    4th lessong homework

---
+++ calculatorHomework/Program.cs
@@ -0,0 +1,61 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        double firstNumber, secondNumber, result;
+        char sign;
+
+        Console.WriteLine("Hi, user. Please, enter first number.");
+        firstNumber = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Please, enter operation character: +, -, * or /.");
+        sign = Convert.ToChar(Console.ReadLine());
+        Console.WriteLine("Please, enter second number.");
+        secondNumber = Convert.ToDouble(Console.ReadLine());
+        if (sign == '+')
+        {
+            result = firstNumber + secondNumber;
+            Console.WriteLine("The sum of your values is: " + result);
+            Console.WriteLine("Press any button to exit.");
+            Console.ReadKey();
+        }
+        else if (sign == '-')
+        {
+            result = firstNumber - secondNumber;
+            Console.WriteLine("The result of your subtraction is: " + result);
+            Console.WriteLine("Press any button to exit.");
+            Console.ReadKey();
+        }
+        else if (sign == '*')
+        {
+            result = firstNumber * secondNumber;
+            Console.WriteLine("The result of your subtraction is: " + result);
+            Console.WriteLine("Press any button to exit.");
+            Console.ReadKey();
+        }
+        else if (sign == '/')
+        {
+            if (secondNumber == 0)
+            {
+                Console.WriteLine("Error. Divider cannot be 0.");
+                Console.WriteLine("Press any button to exit.");
+                Console.ReadKey();
+            }
+            else
+            {
+                result = firstNumber / secondNumber;
+                Console.WriteLine("The result of your division is: " + result);
+                Console.WriteLine("Press any button to exit.");
+                Console.ReadKey();
+            }
+        }
+        else
+        {
+            Console.WriteLine("You entered incorrect value.");
+            Console.WriteLine("Press any button to exit.");
+            Console.ReadKey();
+        }
+
+
+
+    }
+}
---
+++ weekdaysHomework/Program.cs
@@ -0,0 +1,32 @@
+﻿
+Console.WriteLine("Hi, user. specify number from 1 to 7 and click enter to know a weekday.");
+int.TryParse(Console.ReadLine(), out int dayNumber);
+
+    switch (dayNumber)
+    {
+        case 1:
+            Console.WriteLine("Weekday is Monday.");
+            break;
+        case 2:
+            Console.WriteLine("Weekday is Tuesday.");
+            break;
+        case 3:
+            Console.WriteLine("Weekday is Wednesday.");
+            break;
+        case 4:
+            Console.WriteLine("Weekday is Thursday.");
+            break;
+        case 5:
+            Console.WriteLine("Weekday is Friday.");
+            break;
+        case 6:
+            Console.WriteLine("Weekday is Saturday.");
+            break;
+        case 7:
+            Console.WriteLine("Weekday is Sunday");
+            break;
+        default:
+            Console.WriteLine("Incorrect input.");
+            break;
+
+    }
