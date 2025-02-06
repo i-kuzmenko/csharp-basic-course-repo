74648bbc1db804360d2d00b5f1313bc4ff0230e4
Ivan <kuzmenko.ivan.qa@gmail.com>
2025-02-06 17:51:55 +0100

    homeworks with error mesages

---
+++ divisionByZero/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        double firstNumber, secondNumber, result;
+        char sign;
+
+        Console.WriteLine("Hi, user. Please, enter first number.");
+        firstNumber = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Please, enter second number.");
+        secondNumber = Convert.ToDouble(Console.ReadLine());
+
+        if (secondNumber == 0)
+        {
+            Console.WriteLine("Error. Divider cannot be 0.");
+            Console.WriteLine("Press any button to exit.");
+            Console.ReadKey();
+        }
+        else if (secondNumber != 0)
+        {
+            result = firstNumber / secondNumber;
+            Console.WriteLine("The result of your division is: " + result);
+            Console.WriteLine("Press any button to exit.");
+            Console.ReadKey();
+        }
+        else
+        {
+            Console.WriteLine("You entered incorrect value.");
+            Console.WriteLine("Press any button to exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ notDigitErrorHomework/Program.cs
@@ -0,0 +1,16 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        if (int.TryParse(Console.ReadLine(), out int number))
+        {
+            Console.WriteLine($"You entered: {number}");
+        }
+        else
+        {
+            Console.WriteLine("Error: Invalid input! Please enter a valid number.");
+        }
+    }
+}
