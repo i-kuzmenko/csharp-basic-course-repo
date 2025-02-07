291f50d406247b87d095498300607331ebadbebc
Ivan <kuzmenko.ivan.qa@gmail.com>
2025-02-07 10:00:32 +0100

    updated with try catch

--- divisionByZero/Program.cs
+++ divisionByZero/Program.cs
@@ -4,30 +4,31 @@
     {
         double firstNumber, secondNumber, result;
         char sign;
-
-        Console.WriteLine("Hi, user. Please, enter first number.");
-        firstNumber = Convert.ToDouble(Console.ReadLine());
-        Console.WriteLine("Please, enter second number.");
-        secondNumber = Convert.ToDouble(Console.ReadLine());
-
-        if (secondNumber == 0)
+        try
         {
-            Console.WriteLine("Error. Divider cannot be 0.");
-            Console.WriteLine("Press any button to exit.");
-            Console.ReadKey();
+            Console.WriteLine("Hi, user. Please, enter first number.");
+            firstNumber = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please, enter second number.");
+            secondNumber = Convert.ToDouble(Console.ReadLine());
+            if (secondNumber == 0)
+            {
+                Console.WriteLine("Error. Divider cannot be 0.");
+                Console.WriteLine("Press any button to exit.");
+                Console.ReadKey();
+            }
+            else if (secondNumber != 0)
+            {
+                result = firstNumber / secondNumber;
+                Console.WriteLine("The result of your division is: " + result);
+                Console.WriteLine("Press any button to exit.");
+                Console.ReadKey();
+            }
         }
-        else if (secondNumber != 0)
-        {
-            result = firstNumber / secondNumber;
-            Console.WriteLine("The result of your division is: " + result);
-            Console.WriteLine("Press any button to exit.");
-            Console.ReadKey();
-        }
-        else
+        catch (FormatException)
         {
             Console.WriteLine("You entered incorrect value.");
-            Console.WriteLine("Press any button to exit.");
-            Console.ReadKey();
         }
+        
+        
     }
 }
